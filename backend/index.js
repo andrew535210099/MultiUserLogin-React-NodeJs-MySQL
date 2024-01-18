6eddd3ad26d897922065d13d11f1b1fa7929e850
andrew535210099 <andrew.535210099@stu.untar.ac.id>
2024-01-18 19:57:28 +0700

    Membuat folder backend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(
+  session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookies: {
+      secure: "auto",
+      // kalau https true kalau http false
+    },
+  })
+);
+// Middleware
+app.use(
+  cors({
+    // supaya bisa bertukar data seperti cookies
+    credentials: true,
+    // domain untuk mengizinkan akses API kita bisa berupa array
+    origin: "http://localhost:3000",
+  })
+);
+
+// supaya bisa menerima dalam format json
+app.use(express.json());
+
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server up and running...");
+});
